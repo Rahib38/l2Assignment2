461fb14049a5d53bb563d35f020e2829472e5e69
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-22 12:16:04 -0800

    added eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.node,
+    process:"readonly"
+   }}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions":"error",
+      "perfect-const":"error",
+      "no-console":"warn",
+      "no-unfef":"error",
+    },
+  },
+];
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
-// const port = ;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
